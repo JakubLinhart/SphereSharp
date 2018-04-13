54959936e6c679952a38ca877ffd21437afcef1d
Jakub Linhart <jakub.linhart@gmail.com>
2018-04-13 15:40:27 +0200

    Template section implemented.

--- SphereSharp/Syntax/SectionParser.cs
+++ SphereSharp/Syntax/SectionParser.cs
@@ -67,6 +67,8 @@ namespace SphereSharp.Syntax
                     return SkillSectionParser.ParseSkillDef(sectionType, sectionName);
                 case "spell":
                     return SpellSectionParser.ParseSpell(sectionType, sectionName);
+                case "template":
+                    return TemplateSectionParser.ParseTemplate(sectionType, sectionName);
                 case "events":
                     return EventsSectionParser.ParseEvents(sectionType, sectionName);
                 default:
---
+++ SphereSharp/Syntax/TemplateSectionSyntax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+
+namespace SphereSharp.Syntax
+{
+    public class TemplateSectionSyntax : SectionSyntax
+    {
+        public ImmutableArray<PropertySyntax> Properties { get; }
+
+        public TemplateSectionSyntax(string type, string name, ImmutableArray<PropertySyntax> properties)
+            : base(type, name, null)
+        {
+            Properties = properties;
+        }
+
+        public string GetPropertyValue(string propertyName)
+        {
+            return Properties.FirstOrDefault(x => x.LValue.Equals(propertyName, StringComparison.OrdinalIgnoreCase))?.RValue;
+        }
+    }
+}
