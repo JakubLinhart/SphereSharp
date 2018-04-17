e0ade703f83142e6a4fb90ec58db8b65b887252f
Jakub Linhart <jakub.linhart@gmail.com>
2018-04-17 11:28:07 +0200

    Generator's command line arguments.

--- SphereSharp.Sphere.Generator/Program.cs
+++ SphereSharp.Sphere.Generator/Program.cs
@@ -12,11 +12,19 @@ namespace SphereSharp.Sphere.Generator
     {
         static void Main(string[] args)
         {
-            var syntax = FileSyntax.Parse("asdf", File.ReadAllText(@"c:\Users\jakub\sources\ultima\erebor\sphere\scripts\funkce.scp"));
+            if (args.Length != 2)
+            {
+                Console.WriteLine("<input file> <output file>");
+                return;
+            }
+
+            var syntax = FileSyntax.Parse("asdf", File.ReadAllText(args[0]));
             var generator = new Sphere99Generator();
             generator.Visit(syntax);
 
-            Console.WriteLine(generator.ToString());
+            string generatedSourceCode = generator.ToString();
+            Console.WriteLine(generatedSourceCode);
+            File.WriteAllText(args[1], generatedSourceCode);
         }
     }
 }
