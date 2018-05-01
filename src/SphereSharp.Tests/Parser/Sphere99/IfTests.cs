ba28e8691f16698cd4d8f3dee579dbe861ccf4e6
SirGlorg <sirglorg@gmail.com>
2018-05-01 19:32:26 +0200

    EOF ignore & empty IFs

--- src/SphereSharp.Tests/Parser/Sphere99/IfTests.cs
+++ src/SphereSharp.Tests/Parser/Sphere99/IfTests.cs
@@ -81,6 +81,67 @@ else
 endif");
         }
 
+        [TestMethod]
+        public void Can_parse_empty_if()
+        {
+            CheckStructure("if(0);else(1);endif;", @"if 1
+else
+    call1
+endif");
+        }
+
+        [TestMethod]
+        public void Can_parse_empty_if_with_comment()
+        {
+            CheckStructure("if(1);else(1);endif;", @"if 1
+    // call1
+else
+    call2
+endif");
+        }
+
+        [TestMethod]
+        public void Can_parse_empty_elseif()
+        {
+            CheckStructure("if(1);elseif(0);else(1);endif;", @"if 1
+    call1
+elseif 2
+else
+    call2
+endif");
+        }
+
+        [TestMethod]
+        public void Can_parse_empty_elseif_with_comment()
+        {
+            CheckStructure("if(1);elseif(0);else(1);endif;", @"if 1
+    call1
+elseif 2
+    // call2
+else
+    call3
+endif");
+        }
+
+        [TestMethod]
+        public void Can_parse_empty_else()
+        {
+            CheckStructure("if(1);else(0);endif;", @"if 1
+    call1
+else
+endif");
+        }
+
+        [TestMethod]
+        public void Can_parse_empty_else_with_comment()
+        {
+            CheckStructure("if(1);else(0);endif;", @"if 1
+    call1
+else
+    // call2
+endif");
+        }
+
         private void ShouldSucceed(string src) => Parse(src, parser => parser.ifStatement());
 
         private void CheckStructure(string expectedResult, string src)
@@ -100,7 +161,10 @@ endif");
 
             public override bool VisitIfStatement([NotNull] sphereScript99Parser.IfStatementContext context)
             {
-                result.Append($"if({context.codeBlock().statement().Length});");
+                if (context.codeBlock() != null)
+                    result.Append($"if({context.codeBlock().statement().Length});");
+                else
+                    result.Append($"if(0);");
 
                 base.VisitIfStatement(context);
                 result.Append("endif;");
@@ -110,7 +174,20 @@ endif");
 
             public override bool VisitElseStatement([NotNull] sphereScript99Parser.ElseStatementContext context)
             {
-                result.Append($"else({context.codeBlock().statement().Length});");
+                if (context.codeBlock() != null)
+                    result.Append($"else({context.codeBlock().statement().Length});");
+                else
+                    result.Append($"else(0);");
+
+                return false;
+            }
+
+            public override bool VisitElseIfStatement([NotNull] sphereScript99Parser.ElseIfStatementContext context)
+            {
+                if (context.codeBlock() != null)
+                    result.Append($"elseif({context.codeBlock().statement().Length});");
+                else
+                    result.Append($"elseif(0);");
 
                 return false;
             }
--- src/SphereSharp/sphereScript99.g4
+++ src/SphereSharp/sphereScript99.g4
@@ -1,18 +1,18 @@
 ﻿grammar sphereScript99;
 
-file: NEWLINE? section+ eofSection? NEWLINE? EOF;
+file: NEWLINE? section+ (eofSection | EOF);
 
 section: functionSection;
-eofSection: EOF_SECTION_HEADER (NEWLINE | EOF);
+eofSection: EOF_SECTION_HEADER;
 functionSection: functionSectionHeader codeBlock;
 functionSectionHeader: FUNCTION_SECTION_HEADER_START SYMBOL ']' (NEWLINE | EOF);
 codeBlock: statement+;
 
 statement: WS*? (call | assignment | ifStatement) (NEWLINE | EOF);
 
-ifStatement: IF WS+ evalExpression (NEWLINE | EOF) codeBlock (elseIfStatement)* elseStatement? WS* ENDIF ;
-elseIfStatement: WS* ELSEIF WS+ evalExpression (NEWLINE | EOF) codeBlock;
-elseStatement: WS* ELSE (NEWLINE | EOF) codeBlock;
+ifStatement: IF WS+ evalExpression (NEWLINE | EOF) codeBlock? (elseIfStatement)* elseStatement? WS* ENDIF ;
+elseIfStatement: WS* ELSEIF WS+ evalExpression (NEWLINE | EOF) codeBlock?;
+elseStatement: WS* ELSE (NEWLINE | EOF) codeBlock?;
 
 macro: LESS_THAN memberAccess MORE_THAN ;
 call: memberAccess;
