069e0fcf0d6c31ee0028149c0acdc24e270d53e9
Jakub Linhart <jakub.linhart@gmail.com>
2018-07-06 11:26:21 +0200

    Measuring command execution time.

--- src/SphereSharp.Cli/Program.cs
+++ src/SphereSharp.Cli/Program.cs
@@ -8,6 +8,7 @@ using SphereSharp.Cli.TranspileSave;
 using SphereSharp.Cli.TranspileShard;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -20,12 +21,27 @@ namespace SphereSharp.Cli
         static void Main(string[] args)
         {
             CommandLine.Parser.Default.ParseArguments<RoundtripOptions, TranspileOptions, TranspileSaveOptions, TranspileShardOptions, CreateShardSettingsOptions, AnalyzeShardOptions>(args)
-                .WithParsed<RoundtripOptions>(options => new RoundtripCommand().Roundtrip(options))
-                .WithParsed<TranspileOptions>(options => new TranspileCommand().Transpile(options))
-                .WithParsed<TranspileSaveOptions>(options => new TranspileSaveCommand().Transpile(options))
-                .WithParsed<TranspileShardOptions>(options => new TranspileShardCommand().Transpile(options))
-                .WithParsed<CreateShardSettingsOptions>(options => new CreateShardSettingsCommand().Create(options))
-                .WithParsed<AnalyzeShardOptions>(options => new AnalyzeShardCommand().Analyze(options));
+                .WithParsed<RoundtripOptions>(options => ExecuteCommand(() => new RoundtripCommand().Roundtrip(options)))
+                .WithParsed<TranspileOptions>(options => ExecuteCommand(() => new TranspileCommand().Transpile(options)))
+                .WithParsed<TranspileSaveOptions>(options => ExecuteCommand(() => new TranspileSaveCommand().Transpile(options)))
+                .WithParsed<TranspileShardOptions>(options => ExecuteCommand(() => new TranspileShardCommand().Transpile(options)))
+                .WithParsed<CreateShardSettingsOptions>(options => ExecuteCommand(() => new CreateShardSettingsCommand().Create(options)))
+                .WithParsed<AnalyzeShardOptions>(options => ExecuteCommand(() => new AnalyzeShardCommand().Analyze(options)));
+        }
+
+        private static void ExecuteCommand(Action commandAction)
+        {
+            var watch = Stopwatch.StartNew();
+            try
+            {
+                commandAction();
+            }
+            finally
+            {
+                watch.Stop();
+                Console.WriteLine();
+                Console.WriteLine($"Execution time: {watch.Elapsed}");
+            }
         }
     }
 }
